4baea8d1a9b0ab16d9c801ee18850724685366ef
aleares <entarwen07@hotmail.com>
2021-11-04 12:03:47 -0500

    Filtrado del menú según tipo y corrección de distribución de espacio para las tarjetas

--- burgerQueen/src/app/components/waiter-view/waiter-view.component.ts
+++ burgerQueen/src/app/components/waiter-view/waiter-view.component.ts
@@ -1,33 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../../rest.service';
 
-
 @Component({
   selector: 'app-waiter-view',
   templateUrl: './waiter-view.component.html',
-  styleUrls: ['./waiter-view.component.css']
+  styleUrls: ['./waiter-view.component.css'],
 })
 export class WaiterViewComponent implements OnInit {
-  
-  constructor(private RestService: RestService,) { }
-  
+  constructor(private RestService: RestService) {}
+
   ngOnInit(): void {
     this.cargarData();
     this.screenWidth = window.innerWidth;
+    console.log(this.screenWidth);
   }
+  typeArrayMenu: any = [];
 
-  public cargarData(){
-    this.RestService.get('../assets/json/aquelarreMenu.json')
-    .subscribe(res => {
-      console.log(res);
-      this.menuArray = res;
-    })
-  }
-  
   clientName = '';
   tableNumber = '';
   public menuArray: any = [];
   public screenWidth: any;
 
+  statedMenu = 'breakfast';
+  public cargarData() {
+    this.RestService.get('../assets/json/aquelarreMenu.json').subscribe(
+      (res) => {
+        console.log(res);
+        this.menuArray = res;
+        this.typeArrayMenu = this.menuArray.menu.filter(
+          (dish: { type: string }) => dish.type === this.statedMenu
+        );
+      }
+    );
+  }
 
+  switchMenu(typeMenu: string) {
+    this.statedMenu = typeMenu;
+    console.log(
+      'Hola',
+      this.menuArray.menu.filter(
+        (dish: { type: string }) => dish.type === this.statedMenu
+      )
+    );
+    this.typeArrayMenu = this.menuArray.menu.filter(
+      (dish: { type: string }) => dish.type === this.statedMenu
+    );
+  }
 }
